95fb34f3c4ff620acefbb93d36dd74d3b82f9e2b
Bas Wind <none@none>
2013-02-20 13:20:21 +0100

    Changed version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Propeller',
-    version='0.1.0',
+    version='0.1.1',
     author='Bas Wind',
     author_email='mailtobwind@gmail.com',
     packages=['propeller'],
